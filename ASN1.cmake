3d96b2a09f203e7ddc6632516067f5204a0b899e
Timothy Madden <terminatorul@gmail.com>
2016-03-21 20:53:17 +0200

    Add .cmake script for ASN.1 module library
    CMake script to add INTERFACE IMPORT target libraries with C source
    files generated from ASN.1 Module Definition files with external `asn1c`
    command.
    
    See X.680:
    https://www.itu.int/rec/dologin_pub.asp?lang=e&id=T-REC-X.680-201508-I!!PDF-E&type=items
    
    Add markdown README and BSD 3-clause license.

---
+++ ASN1.cmake
@@ -0,0 +1,246 @@
+set(ASN1C_MODULE_LIST_FILE ${CMAKE_CURRENT_LIST_FILE})
+
+# Generates C source files for the given ASN.1 module definition files,
+# using the `asn1c` compiler, see http://lionet.info/asn1c/
+#
+# Creates the given library target, that can be linked with in order to 
+# include generated sources and the needed options to your project.
+#
+# Sample usage:
+#
+#   asn1_modules_library(x509
+#           GENERATED_SOURCE_DIRECTORY "${PROJECT_BINARY_DIR}/gensrc"
+#           INCLUDE_PREFIX_DIRECTORY "asn1"
+#           ASN1C_OPTIONS -fwide-types -fincludes-quoted -print-lines
+#           ASN1_DEBUG_OUTPUT false
+#           DISABLE_WARNINGS "-"
+#           GLOBAL_TARGET
+#           MODULES "ModuleDefinitionFile.asn1" "ModuleDefinitionFile.asn1")
+#
+# target_link_libraries(... x509)
+
+function(asn1_add_module_library ASN1_LIBRARY_TARGET)
+
+    # local variables for function arguments
+    set(ASN1_EMIT_DEBUG false)
+    set(ASN1_INCLUDE_PREFIX "-NOTFOUND")
+    set(ASN1_BASE_OUTPUT_DIR)
+    set(ASN1_COMPILE_FLAGS)
+    set(ASN1_CMD_OPTIONS)
+    set(ASN1_MODULES)
+    set(ASN1_ARG_SELECTOR)
+    set(ASN1_GLOBAL_TARGET)
+    
+    # parse function arguments
+    foreach(ASN1_ARG ${ARGN})
+        if     (ASN1_ARG_SELECTOR STREQUAL "basepath")
+            set(ASN1_BASE_OUTPUT_DIR ${ASN1_ARG})
+            set(ASN1_ARG_SELECTOR)
+        elseif (ASN1_ARG_SELECTOR STREQUAL "incprefix")
+            set(ASN1_INCLUDE_PREFIX ${ASN1_ARG})
+            set(ASN1_ARG_SELECTOR)
+        elseif (ASN1_ARG_SELECTOR STREQUAL "debug")
+            set(ASN1_EMIT_DEBUG ${ASN1_ARG})
+            set(ASN1_ARG_SELECTOR)
+        else()
+            string(TOUPPER ${ASN1_ARG} ASN1_ARG_UPPER)
+
+            if (ASN1_ARG_UPPER STREQUAL "GENERATED_SOURCE_DIRECTORY")
+                set(ASN1_ARG_SELECTOR "basepath")
+            elseif(ASN1_ARG_UPPER STREQUAL "INCLUDE_PREFIX_DIRECTORY")
+                set(ASN1_ARG_SELECTOR "incprefix")
+            elseif(ASN1_ARG_UPPER STREQUAL "ASN1C_OPTIONS")
+                set(ASN1_ARG_SELECTOR "opts")
+            elseif(ASN1_ARG_UPPER STREQUAL "COMPILE_FLAGS")
+                set(ASN1_ARG_SELECTOR "cflags")
+            elseif(ASN1_ARG_UPPER STREQUAL "DISABLE_WARNINGS")
+                set(ASN1_ARG_SELECTOR "warn")
+            elseif(ASN1_ARG_UPPER STREQUAL "ASN1_DEBUG_OUTPUT")
+                set(ASN1_ARG_SELECTOR "debug")
+            elseif(ASN1_ARG_UPPER STREQUAL "GLOBAL_TARGET")
+                set(ASN1_GLOBAL_TARGET "GLOBAL")
+                set(ASN1_ARG_SELECTOR)
+            elseif(ASN1_ARG_UPPER STREQUAL "MODULES")
+                set(ASN1_ARG_SELECTOR)
+            else()
+                if(ASN1_ARG_SELECTOR STREQUAL "opts")
+                    list(APPEND ASN1_CMD_OPTIONS ${ASN1_ARG})
+                elseif(ASN1_ARG_SELECTOR STREQUAL "cflags")
+                    list(APPEND ASN1_COMPILE_FLAGS ${ASN1_ARG})
+                elseif(ASN1_ARG_SELECTOR STREQUAL "warn")
+                    if(ASN1_ARG STREQUAL "-")
+                        if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC" OR CMAKE_C_COMPILER_ID STREQUAL "MSVC")
+                            list(APPEND ASN1_COMPILE_FLAGS "/w")
+                        elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_C_COMPILER_ID STREQUAL "GNU")
+                            list(APPEND ASN1_COMPILE_FLAGS "-w")
+                        endif()
+                    elseif(ASN1_ARG STREQUAL "MSVC")
+                        if(MSVC)
+                            list(APPEND ASN1_COMPILE_FLAGS "/w")
+                        endif()
+                    elseif(ASN1_ARG STREQUAL "GNU")
+                        if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_C_COMPILER_ID STREQUAL "GNU")
+                            list(APPEND ASN1_COMPILE_FLAGS "-w")
+                        endif()
+                    else()
+                        message(WARNING "A known compiler is expected in order to disable warnings, ${ASN1_ARG} given.")
+                    endif()
+                else()
+                    if(NOT IS_ABSOLUTE ${ASN1_ARG})
+                        set(ASN1_ARG "${PROJECT_SOURCE_DIR}/${ASN1_ARG}")
+                    endif()
+    
+                    list(APPEND ASN1_MODULES ${ASN1_ARG})
+                endif()
+            endif()
+        endif()
+    endforeach()
+
+    # apply defaults and verify the command options
+    if(NOT ASN1_BASE_OUTPUT_DIR)
+        set(ASN1_BASE_OUTPUT_DIR "${PROJECT_BINARY_DIR}/gensrc")
+    endif()
+
+    if (NOT IS_ABSOLUTE ${ASN1_BASE_OUTPUT_DIR})
+        set(ASN1_BASE_OUTPUT_DIR "${PROJECT_BINARY_DIR}/${ASN1_BASE_OUTPUT_DIR}")
+    endif()
+
+    if (ASN1_INCLUDE_PREFIX STREQUAL "-NOTFOUND")
+        set(ASN1_INCLUDE_PREFIX "asn1")
+    endif()
+
+    if (ASN1_INCLUDE_PREFIX STREQUAL "-")
+        set(ASN1_INCLUDE_PREFIX "")
+    endif()
+
+    if (IS_ABSOLUTE ${ASN1_INCLUDE_PREFIX})
+        message(FATAL_ERROR "Absolute path ${ASN1_INCLUDE_PREFIX} given as include prefix directory for generated ASN1 sources.")
+    endif()
+
+    set(ASN1_WORKING_DIRECTORY "${ASN1_BASE_OUTPUT_DIR}/${ASN1_INCLUDE_PREFIX}")
+
+    set(ASN1_GENERATE_SOURCES false)
+
+    if (NOT EXISTS "${ASN1_WORKING_DIRECTORY}")
+        file(MAKE_DIRECTORY ${ASN1_WORKING_DIRECTORY})
+        set(ASN1_GENERATE_SOURCES true)
+    endif()
+
+    if(NOT ASN1_MODULES)
+        message(FATAL_ERROR "No ASN.1 modules given for target ASN1::${ASN1_LIBRARY_TARGET}.")
+    endif()
+
+    # walk through the modules list and create list of expected hashes
+    set(ASN1_CMD_MODULES)
+    set(ASN1_EXPECTED_BUILD_ID)
+    set(ASN1_BUILD_TAG_FILE "${ASN1_BASE_OUTPUT_DIR}/${ASN1_LIBRARY_TARGET}.build.tag")
+    set(ASN1_LIBRARY_TARGET "ASN1::${ASN1_LIBRARY_TARGET}")
+
+    # check CMake module file
+    if("${ASN1C_MODULE_LIST_FILE}" IS_NEWER_THAN "${ASN1_BUILD_TAG_FILE}")
+        set(ASN1_GENERATE_SOURCES true)
+    endif()
+
+    file(SHA3_384 ${ASN1C_MODULE_LIST_FILE} ASN1_EXPECTED_BUILD_ID)
+
+    # check `asn1c` compiler executable
+    find_program(ASN1C_EXE asn1c)
+
+    if (NOT ASN1C_EXE)
+        message(FATAL_ERROR "ASN.1 compiler `asn1c` (see http://lionet.info/asn1c/) is needed to compile module definitions for target ${ASN1_LIBRARY_TARGET}.")
+    endif()
+
+    if("${ASN1C_EXE}" IS_NEWER_THAN "${ASN1_BUILD_TAG_FILE}")
+        set(ASN1_GENERATE_SOURCES true)
+    endif()
+
+    file(SHA3_384 ${ASN1C_EXE} ASN1C_EXE_HASH)
+    list(APPEND ASN1_EXPECTED_BUILD_ID ${ASN1C_EXE_HASH})
+
+    # check build options together with the hashes
+    list(APPEND ASN1_EXPECTED_BUILD_ID ${ASN1_CMD_OPTIONS})
+
+    # check module definitions
+    foreach(ASN1_MODULE ${ASN1_MODULES})
+        if(NOT EXISTS ${ASN1_MODULE})
+            message(FATAL_ERROR "Missing ASN.1 module definition file ${ASN1_MODULE}")
+        endif()
+
+        if("${ASN1_MODULE}" IS_NEWER_THAN "${ASN1_BUILD_TAG_FILE}")
+            set(ASN1_GENERATE_SOURCES true)
+        endif()
+
+        file(SHA3_384 ${ASN1_MODULE} ASN1_MODULE_HASH)
+        list(APPEND ASN1_EXPECTED_BUILD_ID ${ASN1_MODULE_HASH})
+
+        file(RELATIVE_PATH ASN1_CMD_MODULE ${ASN1_WORKING_DIRECTORY} ${ASN1_MODULE})
+        list(APPEND ASN1_CMD_MODULES ${ASN1_CMD_MODULE})
+    endforeach()
+
+    # check previous build hashes and options
+    if (NOT ASN1_GENERATE_SOURCES)
+        file(READ ${ASN1_BUILD_TAG_FILE} ASN1_PREVIOUS_BUILD_ID)
+
+        if (NOT ASN1_EXPECTED_BUILD_ID STREQUAL ASN1_PREVIOUS_BUILD_ID)
+            set(ASN1_GENERATE_SOURCES true)
+        endif()
+    endif()
+
+    # invoke `asn1c` compiler to generate C sources from ASN.1 module definitions
+    if(ASN1_GENERATE_SOURCES)
+        file(REMOVE ${ASN1_BUILD_TAG_FILE})
+        file(REMOVE_RECURSE ${ASN1_WORKING_DIRECTORY})
+        file(MAKE_DIRECTORY ${ASN1_WORKING_DIRECTORY})
+
+        execute_process(COMMAND "${ASN1C_EXE}" ${ASN1_CMD_OPTIONS} ${ASN1_CMD_MODULES}
+                        WORKING_DIRECTORY ${ASN1_WORKING_DIRECTORY}
+                        RESULT_VARIABLE ASN1_EXIT_CODE)
+
+        if (NOT ASN1_EXIT_CODE EQUAL 0)
+            message(FATAL_ERROR "Source code generation for ${ASN1_LIBRARY_TARGET} target with `asn1c` compiler failed "
+                    "with exit code ${ASN1_EXIT_CODE}.")
+        endif()
+
+        file(WRITE ${ASN1_BUILD_TAG_FILE} "${ASN1_EXPECTED_BUILD_ID}")
+    endif()
+
+    # treat module files as configure files, so they trigger cmake generation after content changes
+    foreach(ASN1_MODULE ${ASN1_MODULES})
+        get_filename_component(ASN1_MODULE_BASENAME ${ASN1_MODULE} NAME)
+        configure_file(${ASN1_MODULE} "${ASN1_BASE_OUTPUT_DIR}/${ASN1_MODULE_BASENAME}.build.tag" COPYONLY)
+    endforeach()
+
+    # list generated files
+    file(GLOB_RECURSE ASN1_GENERATED_SOURCES
+            "${ASN1_WORKING_DIRECTORY}/*.c"
+            "${ASN1_WORKING_DIRECTORY}/*.h"
+            "${ASN1_WORKING_DIRECTORY}/*.C"
+            "${ASN1_WORKING_DIRECTORY}/*.H"
+            "${ASN1_WORKING_DIRECTORY}/*.cc"
+            "${ASN1_WORKING_DIRECTORY}/*.hh"
+            "${ASN1_WORKING_DIRECTORY}/*.cxx"
+            "${ASN1_WORKING_DIRECTORY}/*.hxx"
+            "${ASN1_WORKING_DIRECTORY}/*.cpp"
+            "${ASN1_WORKING_DIRECTORY}/*.hpp")
+
+    foreach(ASN1_GENERATED_MAIN_FILE "${ASN1_WORKING_DIRECTORY}/converter-sample.c" "${ASN1_WORKING_DIRECTORY}/converter-sample.h"
+                                    "${ASN1_WORKING_DIRECTORY}/converter-example.h" "${ASN1_WORKING_DIRECTORY}/converter-example.c")
+        list(REMOVE_ITEM ASN1_GENERATED_SOURCES ${ASN1_GENERATED_MAIN_FILE})
+    endforeach()
+
+    # create library target
+    add_library(${ASN1_LIBRARY_TARGET} INTERFACE IMPORTED ${ASN1_GLOBAL_TARGET})
+    target_include_directories(${ASN1_LIBRARY_TARGET} INTERFACE ${ASN1_BASE_OUTPUT_DIR} ${ASN1_WORKING_DIRECTORY})
+    if (ASN1_EMIT_DEBUG)
+        target_compile_definitions(${ASN1_LIBRARY_TARGET} INTERFACE "-DASN1_EMIT_DEBUG=1")
+    endif()
+    target_sources(${ASN1_LIBRARY_TARGET} INTERFACE ${ASN1_MODULES} ${ASN1_GENERATED_SOURCES})
+    set_source_files_properties(${ASN1_GENERATED_SOURCES} PROPERTIES GENERATED true)
+    set_source_files_properties(${ASN1_MODULES} PROPERTIES HEADER_FILE_ONLY true)
+
+    if (ASN1_COMPILE_FLAGS)
+        foreach(ASN1_GENERATED_SOURCE ${ASN1_GENERATED_SOURCES})
+            set_property(SOURCE ${ASN1_GENERATED_SOURCE} APPEND_STRING PROPERTY COMPILE_FLAGS ${ASN1_COMPILE_FLAGS})
+        endforeach()
+    endif()
+endfunction()
---
+++ README.md
@@ -0,0 +1,64 @@
+# ASN1.cmake
+
+A CMake include file (TODO: module) that can run the external `asn1c` compiler on ASN.1 Module Definition ([X.680](https://www.itu.int/rec/dologin_pub.asp?lang=e&id=T-REC-X.680-201508-I!!PDF-E&type=items)) files and generate C source files. For ASN.1 compiler see <http://lionet.info/asn1c>.
+
+## Basic usage
+The CMake script file allows you to include the ASN.1 module definition files as sources in your `CMakeLists.txt`. In the list file you can create an ASN.1 module library, and use it in your project in a call to [`target_link_libraries()`](https://cmake.org/cmake/help/latest/command/target_link_libraries.html).
+
+For example:
+```
+    include(ASN1.cmake)
+
+    asn1_add_module_library(encoderLib ModuleDefinitionFile.asn1 NextModuleDefinition.asn1)
+
+    #....
+
+    target_link_libraries(... ASN1::encoderLib)
+```
+
+Notice how the resulting library name receives the `ANS1::` prefix applied to the name given in the first argument. To include the generated headers in your C/C++ code, use a line like:
+```
+#include <asn1/NumericString.h>
+```
+
+## Syntax
+Use [`file(DOWNLOAD ...)`](https://cmake.org/cmake/help/latest/command/file.html#download) in your `CMakeLists.txt` to download the script if it does not exist.
+
+The full function syntax is as follows:
+```
+include(ASN1.cmake)
+
+asn1_add_module_library(
+	targetName
+	GENERATED_SOURCE_DIRECTORY OutputDirectoryName
+	INCLUDE_PREFIX_DIRECTORY SubdirectoryForIncludes
+	ASN1C_OPTIONS asn1c_opt asn1c_opt ...
+	ASN1_DEBUG_OUTPUT true
+	DISABLE_WARNINGS "-" GNU MSVC ...
+	COMPILE_FLAGS compiler-flag compiler-flag ...
+	GLOBAL_TARGET
+	MODULES ModuleFile.asn1 ModuleFile.asn1 ...)
+```
+
+The `GENERATED_SOURCE_DIRECTORY` keyword specifies the base directory for the location of the generated C source files. The default value is `${PROJECT_BINARY_DIR}/gensrc`. The external `asn1c` command will be run in this directory or a subdirectory named after the include prefix.
+
+The `INCLUDE_PREFIX_DIRECTORY` keyword gives a subdirectory within the `GENERATED_SOURCE_DIRECTORY`, where the C source files will be found.  The intent is that this subdirectory will have to be used as part of the file name in the C/C++ `#include` directives that reference generated files. The default value for the prefix directory is `asn1`. You can use the value `-` if you want no prefix directory for the resulting include files. In this case the generated files will be found directly under the `GENERATED_SOURCE_DIRECTORY`.
+
+The `ASN1C_OPTIONS` keyword gives a list of additional options to be used on the command line to external `asn1c` tool. See the online [asn1c](http://lionet.info/asn1c) documentation for these options. You should not give any options that suppress the normal source generation of the compiler or that change the output directory. To specify the path to `asn1c` executable you can define the CMake variable `ASN1C_EXE`.
+
+The `ASN1_DEBUG_OUTPUT` keyword with the value `true` will add a C preprocessor macro to the compilation options for your target. The macro is used by the generated C sources to output debug information when parsing encoded ASN.1 input. Use this options during debugging to see why the generated code can not parse your encoded input. The effect is to add `-DASN1_EMIT_DEBUG=1` to CMake targets that link with `ASN1::targetName`.
+
+As the function produces generated code, you may want to disable warnings for the resulting sources. Unfortunately the warning options are compiler-specific, and CMake currently has no single option to disable warnings for any supported compiler.  So you can use the `DISABLE_WARNINGS` keyword with a list of compiler ID arguments, for one or more of the well known compilers. They currently are `GNU` and `MSVC` only. Or you can use a `-` to disable warnings on any of these known compilers.
+
+You can pass any list of options to the compiler with the `COMPILE_FLAGS` keyword to the function. The flags listed will be used to compile the generated source files. The `DISABLE_WARNINGS` option above is just a pre-defined set of `COMPILE_FLAGS`.
+
+The `GLOBAL_TARGET` keyword will make the given targetName visible to CMake scripts outside the current source directory. By default the new target `ASN1::targetName` is only visible in or below the current directory, see the CMake documentation for [interface libraries](https://cmake.org/cmake/help/latest/command/add_library.html#id6).
+
+The `MODULES` keyword is only needed after the other list keywords (`ASN1C_OPTIONS`, `COMPILE_FLAGS`...) to terminate that list and start a list of modules to be compiled for C source generation.
+
+Other arguments, that is `ModuleFile.asn1` give ASN.1 Module Definition files as defined in [X.680](https://www.itu.int/rec/dologin_pub.asp?lang=e&id=T-REC-X.680-201508-I!!PDF-E&type=items). File names are relative to [`PROJECT_SOURCE_DIR`](https://cmake.org/cmake/help/latest/variable/PROJECT_SOURCE_DIR.html), to resemble other source files.
+
+## Module library usage
+The function above introduces a new `INTERFACE` library target to your CMake project, with the name given in the first function argument, but with the `ASN1::` prefix. So the resulting target name has the form `ASN1::targetName`. This library can then be added to the link libraries of other project targets, using [`target_link_libraries()`](https://cmake.org/cmake/help/latest/command/target_link_libraries.html). The dependent target will then automatically include the generated source files, that will be compiled with the given compiler options, if any.
+
+Generated source files are never compiled into an actual library for the new target `ASN1::targetName`. This is an `INTERFACE IMPORT` library target, that can only add generated source files, include directories and options to other CMake targets, when used as described. See the CMake documentation for [`INTERFACE IMPORT`](https://cmake.org/cmake/help/latest/command/add_library.html#id6) libraries for details.
